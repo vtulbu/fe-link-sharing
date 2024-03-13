abb003240e8d44c8c5b61ae3e103a2d0d014e701
Tulbu Vadim <tulbu.vadim@gmail.com>
2024-03-13 14:45:08 +0200

    Config colors and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,20 @@
+const colors = require("./src/constants");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		fontFamily: {
+			sans: ["Instrument Sans", ...defaultTheme.fontFamily.sans],
+			serif: [...defaultTheme.fontFamily.serif],
+		},
+		extend: {
+			colors: {
+				...colors
+			}
+		}
 	},
 
 	plugins: []
