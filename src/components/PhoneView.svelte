8549ec411ac6ae200d1c2add144703eb6b1f6a4c
Tulbu Vadim <tulbu.vadim@gmail.com>
2024-03-21 15:30:35 +0200

    Create PhoneView component

---
+++ src/components/PhoneView.svelte
@@ -0,0 +1,11 @@
+<aside class="hidden xl:flex">
+	<img src="icons/illustration-phone-mockup.svg" alt="phone icon" />
+</aside>
+
+<style>
+	aside {
+		padding: 100px 130px;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
