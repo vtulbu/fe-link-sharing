06f7b7648a03b1cd1f95e9bd70a106c15d4a7dc0
Tulbu Vadim <tulbu.vadim@gmail.com>
2024-03-23 15:20:10 +0200

    Add API create account

--- src/routes/(non-logged)/create-account/+page.svelte
+++ src/routes/(non-logged)/create-account/+page.svelte
@@ -3,13 +3,45 @@
 	import NonLoggedText from '../../../components/NonLoggedText.svelte';
 	import InputField from '../../../components/InputField.svelte';
 	import ActionText from '../../../components/ActionText.svelte';
+	import { goto } from '$app/navigation';
+	import axios from 'axios';
+	import { useMutation } from '@sveltestack/svelte-query';
+	import { PUBLIC_API_URL } from '$env/static/public';
+	import { Alert } from 'flowbite-svelte';
+	import { fade } from 'svelte/transition';
+
+	let email = '';
+	let password = '';
+	let isAlertOpen = false;
+
+	const mutation = useMutation(
+		(data: { email: string; password: string }) =>
+			axios.post(`${PUBLIC_API_URL}/auth/create-account`, data),
+		{
+			onSuccess: () => {
+				isAlertOpen = true;
+				const id = setTimeout(() => {
+					isAlertOpen = false;
+					goto('/login');
+				}, 2000);
+			},
+			onError: (error) => {
+				console.error('Error creating account', error);
+			}
+		}
+	);
 </script>
 
 <NonLoggedText heading="Create account" subheading="Let’s get you started sharing your links!" />
-<InputField label="Email address" type="email" placeholder="e.g. alex@email.com">
+<InputField label="Email address" type="email" placeholder="e.g. alex@email.com" bind:value={email}>
 	<img slot="icon" src="/icons/email.svg" alt="email icon" />
 </InputField>
-<InputField label="Password" type="password" placeholder="At least 8 characters">
+<InputField
+	label="Password"
+	type="password"
+	placeholder="At least 8 characters"
+	bind:value={password}
+>
 	<img slot="icon" src="/icons/password.svg" alt="password icon" />
 </InputField>
 
@@ -17,6 +49,20 @@
 	<img slot="icon" src="/icons/password.svg" alt="password icon" />
 </InputField>
 
-<Button onClick={() => console.log('Login clicked')}>Create new account</Button>
+<Button
+	onClick={() =>
+		$mutation.mutate({
+			email,
+			password
+		})}
+>
+	{$mutation.isLoading ? 'Creating account...' : 'Create new account'}
+</Button>
 
 <ActionText href="/login" text="Already have an account?" link="Login" />
+
+{#if isAlertOpen}
+	<Alert color="yellow" transition={fade}>
+		<p>Account created successfully!</p>
+	</Alert>
+{/if}
